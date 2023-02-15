891e220a0158b30c8140071dfd492351ecee7d44
Bryan Sanudo Garcia <bryansanudo95@gmail.com>
2023-02-15 15:58:05 -0500

    navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,11 +28,7 @@ const Navbar = ({ isMenuShown, setIsMenuShown }) => {
     <>
       <div className="fixed w-full h-40 bg-black text-white z-20 ">
         <div className="flex justify-between lg:justify-center md:gap-5 items-center max-w-screen-xl mx-auto px-4 h-full">
-          <a
-            href="https://www.instagram.com/sarazapata144/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://sara-marcas.vercel.app/">
             <div>
               <img
                 src={logoSara}
